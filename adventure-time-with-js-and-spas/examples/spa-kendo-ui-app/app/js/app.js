3e92dce939cc712241ef87a8dc1859892b27e370
bergenhem <cbergenhem@gmail.com>
2013-10-29 18:37:38 -0400

    Add basic Kendo UI SPA framework items

--- adventure-time-with-js-and-spas/examples/spa-kendo-ui-app/app/js/app.js
+++ adventure-time-with-js-and-spas/examples/spa-kendo-ui-app/app/js/app.js
@@ -1,3 +1,27 @@
-window.kendoApp = (function($){
+window.KendoApp = (function($){
+	var _kendoApp = {};
 
+		_kendoApp.startApp = function() {
+
+			var myLayout = new kendo.Layout('kendo-layout-template'); //create our layout
+
+			var myRouter = new kendo.Router(); //initialize our router
+
+			//Views
+			var showView = new kendo.View('show-view'); 
+			var charView = new kendo.View('char-view');
+
+			//Routes
+			myRouter.route('/', function() {
+				myLayout.showIn('#content', showView);
+			});
+			myRouter.route('/chars', function() {
+				myLayout.showIn('#content', charView);
+			});
+
+			myRouter.start();
+			myLayout.render('#app');
+		}
+
+		return _kendoApp;
 })(jQuery);
