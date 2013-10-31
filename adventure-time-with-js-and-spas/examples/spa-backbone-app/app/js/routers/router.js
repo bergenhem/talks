2a742e489223a6258fd1ea1d178046377032f0ec
bergenhem <cbergenhem@gmail.com>
2013-10-30 21:47:57 -0400

    Clean up router code

--- adventure-time-with-js-and-spas/examples/spa-backbone-app/app/js/routers/router.js
+++ adventure-time-with-js-and-spas/examples/spa-backbone-app/app/js/routers/router.js
@@ -1,33 +1,30 @@
 var Workspace = Backbone.Router.extend({
 	routes : {
-		'' : 'getShow',
+		'' : 'show',
+		'chars/' : 'getCharacters',
 		'chars/:name': 'getCharacter',
-		'chars/' : 'getChars',
 		'*actions' : 'defaultRoute' //http://example.com/#whatever
+	},
+	showView: new app.ShowView(),
+	charView: new app.CharsView(),
+	show: function() {
+		this.showView.render();
+	},
+	getCharacters: function() {
+		this.charView.render();
+	},
+	getCharacter: function(name) {
+		this.charView.render();
+	},
+	defaultRoute: function(actions) {
+		this.navigate('#/', { trigger: false });
+		
+		this.showView.render();
 	}
 });
 
+//instantiate our router
 var app_router = new Workspace();
 
-app_router.on('route:getCharacter', function(name) {
-	console.log('/chars/' + name);
-});
-
-app_router.on('route:getChars', function() {
-	console.log('/chars/');
-	this.charsView = new app.CharsView();
-	this.charsView.render();
-});
-
-app_router.on('route:getShow', function() {
-	console.log('/#/')
-	this.showView = new app.ShowView();
-	this.showView.render();
-});
-
-app_router.on('route:defaultRoute', function(actions) {
-	console.log('/' + actions);
-});
-
-//start our router
+//start tracking URL
 Backbone.history.start();
