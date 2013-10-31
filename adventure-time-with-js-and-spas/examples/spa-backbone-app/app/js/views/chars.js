c5615c291c1c462e0ff457ade769a06092ae6471
bergenhem <cbergenhem@gmail.com>
2013-10-31 13:11:33 -0400

    Move collection creation to collection initialize

--- adventure-time-with-js-and-spas/examples/spa-backbone-app/app/js/views/chars.js
+++ adventure-time-with-js-and-spas/examples/spa-backbone-app/app/js/views/chars.js
@@ -6,9 +6,7 @@ app.CharsView = Backbone.View.extend({
 	el: $('#content'),
 	template: _.template($('#characters-template').html()),
 	initialize: function() {
-		var charsList = app.Characters;
-		this.createCharacters(charsList);
-		this.characters = charsList;
+		this.characters = app.Characters;
 	},
 	events: {
 		'change #character-select' : 'characterSelected'
@@ -23,34 +21,6 @@ app.CharsView = Backbone.View.extend({
 	},
 	renderViewByModelId: function(id) {
 
-	},
-	createCharacters: function(charsList) {
-		var finn = new app.Character({
-			id: 0,
-			name: "Finn",
-			species: "Human",
-			occupation: "Hero",
-			description: "Finn (full title: Finn the Human, known as Finn Mertens in the Farmworld timeline and formerly as Pen in the original short) is the main protagonist of the series Adventure Time.",
-			linkUrl: "http://adventuretime.wikia.com/wiki/Finn"
-
-		});
-		var jake = new app.Character({
-			id: 1,
-			name: "Jake",
-			species: "Dog",
-			occupation: "Adventurer",
-			description: "Jake (full title: Jake the Dog), the deuteragonist of Adventure Time, is a magical dog and Finn's constant companion, best friend, and adoptive brother. Jake has Stretchy Powers (which he has had since he was a puppy), which allow him to stretch and manipulate his body, coming in handy on innumerable occasions throughout his and Finn's adventures.",
-			linkUrl: "http://adventuretime.wikia.com/wiki/Jake"
-		});
-		var bmo = new app.Character({
-			id: 2,
-			name: "BMO",
-			species: "MO",
-			occupation: "Video Game System",
-			description: "BMO (phonetically spelled Beemo) is Finn and Jake's living video game console, portable electrical outlet, music player, roommate, camera, alarm clock, toaster, flashlight, strobe light, skateboarder, friend, soccer player, video editor, and video player.",
-			linkUrl: "http://adventuretime.wikia.com/wiki/BMO"
-		});
-		charsList.add([finn, jake, bmo]);
 	},
 	render: function() {
 
