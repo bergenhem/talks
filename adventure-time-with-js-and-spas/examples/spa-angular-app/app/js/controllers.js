b240c0d6efef810593da26b52869cc92ed5cb328
bergenhem <cbergenhem@gmail.com>
2013-11-01 09:43:07 -0400

    Set up controllers for our two views

---
+++ adventure-time-with-js-and-spas/examples/spa-angular-app/app/js/controllers.js
@@ -0,0 +1,11 @@
+var sampleAppControllers = angular.module('sampleAppControllers', []);
+
+sampleAppControllers.controller('ShowController', ['$scope',
+	function ShowController($scope) {
+		//TODO: IMPLEMENT
+	}]);
+
+sampleAppControllers.controller('CharacterController', ['$scope', '$routeParams',
+	function CharacterController($scope, $routeParams) {
+		//TODO: IMPLEMENT
+	}]);
