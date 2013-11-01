993114d4be8378a5383b502aa4bd5d77ad81e3e6
bergenhem <cbergenhem@gmail.com>
2013-11-01 11:11:46 -0400

    Add features to bind the dropdown and update html accordingly

--- adventure-time-with-js-and-spas/examples/spa-angular-app/app/js/app.js
+++ adventure-time-with-js-and-spas/examples/spa-angular-app/app/js/app.js
@@ -7,5 +7,6 @@ sampleAngularApp.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.when('/', { templateUrl: 'partials/show.html', controller: 'ShowController' });
 		$routeProvider.when('/chars', { templateUrl: 'partials/characters.html', controller: 'CharacterController' });
+		$routeProvider.when('/chars/:name', { templateUrl: 'partials/characters.html', controller: 'CharacterController' });
 		$routeProvider.otherwise( { redirectTo: '/' });
 	}]);
