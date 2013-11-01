4c9d53a092597163aaa4af6cd9124417870573ed
bergenhem <cbergenhem@gmail.com>
2013-11-01 10:33:10 -0400

    Set up routes and update to angular 1.2.0-rc3

---
+++ adventure-time-with-js-and-spas/examples/spa-angular-app/app/js/app.js
@@ -0,0 +1,11 @@
+var sampleAngularApp = angular.module('sampleAngularApp', [
+	'ngRoute',
+	'sampleAppControllers'
+	]);
+
+sampleAngularApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.when('/', { templateUrl: 'partials/show.html', controller: 'ShowController' });
+		$routeProvider.when('/chars', { templateUrl: 'partials/characters.html', controller: 'CharacterController' });
+		$routeProvider.otherwise( { redirectTo: '/' });
+	}]);
